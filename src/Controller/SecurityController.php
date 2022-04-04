7d3bf1ba7fd058526237981a8ea07a47c3234d2b
eddesscv <50627300+eddesscv@users.noreply.github.com>
2022-04-04 22:26:32 +0200

    install phpunit

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -4,7 +4,7 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
--- tests/Controller/DefaultControllerTest.php
+++ tests/Controller/DefaultControllerTest.php
@@ -6,13 +6,17 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class DefaultControllerTest extends WebTestCase
 {
-    public function testIndex()
+
+    public function testIndexActionWithoutLogin()
     {
+        // If the user isn't logged, should redirect to the login page
         $client = static::createClient();
+        $client->request('GET', '/');
+        static::assertSame(302, $client->getResponse()->getStatusCode());
 
-        $crawler = $client->request('GET', '/');
-
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
+        $crawler = $client->followRedirect();
+        // Test if login field exists
+        static::assertSame(1, $crawler->filter('input[name="_username"]')->count());
+        static::assertSame(1, $crawler->filter('input[name="_password"]')->count());
     }
 }
